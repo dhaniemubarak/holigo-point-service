3d435958ed78cc976072a1c76878eebe4c2f084f
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-09 15:36:20 +0700

    Update user repository

--- src/main/java/id/holigo/services/holigopointservice/repositories/UserPointRepository.java
+++ src/main/java/id/holigo/services/holigopointservice/repositories/UserPointRepository.java
@@ -1,13 +1,17 @@
 package id.holigo.services.holigopointservice.repositories;
 
-import java.util.Optional;
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import id.holigo.services.holigopointservice.domain.UserPoint;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserPointRepository extends JpaRepository<UserPoint, UUID> {
+public interface UserPointRepository extends JpaRepository<UserPoint, Long> {
 
-    Optional<UserPoint> findByUserId(Long userId);
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE UserPoint u SET u.point=:newPoint WHERE u.userId=:userId AND u.point=:currentPoint")
+    int updatePoint(@Param("userId") Long userId,
+                    @Param("currentDeposit") Integer currentPoint,
+                    @Param("newPoint") Integer newPoint);
 }
