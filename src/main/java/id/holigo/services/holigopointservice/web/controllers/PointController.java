f245db43f9381e03f8165bd0e860298b080bdfe3
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-09 15:37:23 +0700

    Add point controller

---
+++ src/main/java/id/holigo/services/holigopointservice/web/controllers/PointController.java
@@ -0,0 +1,33 @@
+package id.holigo.services.holigopointservice.web.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import id.holigo.services.common.model.PointDto;
+import id.holigo.services.holigopointservice.services.PointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.JMSException;
+
+@RestController
+public class PointController {
+    private PointService pointService;
+
+    @Autowired
+    public void setPointService(PointService pointService) {
+        this.pointService = pointService;
+    }
+
+    @PostMapping("/api/v1/point/debit")
+    public ResponseEntity<PointDto> debit(@RequestBody PointDto pointDto) throws JMSException, JsonProcessingException {
+        return new ResponseEntity<>(pointService.debit(pointDto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/api/v1/point/credit")
+    public ResponseEntity<PointDto> credit(@RequestBody PointDto pointDto) throws JMSException, JsonProcessingException {
+        return new ResponseEntity<>(pointService.credit(pointDto), HttpStatus.CREATED);
+    }
+}
