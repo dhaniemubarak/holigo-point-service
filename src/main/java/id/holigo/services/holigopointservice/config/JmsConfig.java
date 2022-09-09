296fb592b713b75b707ace2d226d5aa27abd96c2
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-09 09:42:54 +0700

    Add JMS Config

---
+++ src/main/java/id/holigo/services/holigopointservice/config/JmsConfig.java
@@ -0,0 +1,26 @@
+package id.holigo.services.holigopointservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String DEBIT_POINT = "debit-point";
+    public static final String CREDIT_POINT = "credit-point";
+
+    public static final String CREATE_ACCOUNT_STATEMENT = "create-account-statement";
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+}
